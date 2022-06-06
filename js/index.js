8f176ac901448a51391a8b34386917732d555524
DavidBolaji <odavidbolaji14@gmail.com>
2022-06-06 16:37:04 +0100

    start

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// const add = document.getElementById("addCount");
+// const remove = document.getElementById("remCount");
+// const nftCount = document.getElementById("nftsNumber");
+// const price = document.getElementById("price");
+
+// const increase = () => {
+//   if (nftCount.innerHTML < 5) {
+//     // +nftCount.textContent + 1;
+//     nftCount.textContent = +nftCount.textContent + 1;
+//     price.textContent = (0.073 * +nftCount.textContent).toFixed(3);
+//   }
+// };
+
+// const decrease = () => {
+//   if (nftCount.innerHTML > 1) {
+//     //   +nftCount.textContent + 1;
+//     nftCount.textContent = +nftCount.textContent - 1;
+//     price.textContent = (0.073 * +nftCount.textContent).toFixed(3);
+//   }
+// };
+
+// add.addEventListener("click", increase);
+// remove.addEventListener("click", decrease);
